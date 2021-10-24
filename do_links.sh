b2dd8b5b81dd190fb48c8bb09bf34e228eb7e499
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2021-10-24 21:42:22 +0900

    added in the links for bash files

--- do_links.sh
+++ do_links.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 read -n 1 -s -r -p "This script may cause issues making links to directories that do not exist yet, like ~/.config/nvim. Install things first! Press any key to continue"
+#TODO can you do these in a loop somehow? need to learn how to bash script...
 ln -s ~/.dotfiles/init.vim ~/.config/nvim/
+ln -s ~/.dotfiles/bash/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/bash/.bash_aliases ~/.bash_aliases
