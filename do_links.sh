bf5a1f8ff75e98a6c850d009248c5c45306c5261
Matt D Brooks <28607360+MattBrooks95@users.noreply.github.com>
2021-11-07 16:11:20 +0900

    little reminder to myself about what to do for dwm. I think I can script it, but I'll do that later

--- do_links.sh
+++ do_links.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read -n 1 -s -r -p "will remove some files before setting up the sym links, also makes dir ~/.config/nvim. Press any key to continue"
+read -n 1 -s -r -p "will remove some files before setting up the sym links, also makes dir ~/.config/nvim. ./dwm/config.h will need to be copied to the dwm directory before doing make & make install. Press any key to continue"
 #TODO can you do these in a loop somehow? need to learn how to bash script...
 mkdir -p ~/.config/nvim
 ln -s ~/.dotfiles/init.vim ~/.config/nvim/
@@ -15,3 +15,4 @@ ln -s ~/.dotfiles/bash/.bash_aliases $bashAliasesLocation
 tmuxConfLocation="$HOME/.tmux.conf"
 rm $tmuxConfLocation
 ln -s ~/.dotfiles/tmux/.tmux.conf $tmuxConfLocation
+
