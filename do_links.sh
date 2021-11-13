95551614631bc41492d8e7ddd56d19e1ac8f4081
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2021-11-13 16:23:51 +0900

    here goes, moving nvim-cmp setup into a lua file.幸運を祈る

--- do_links.sh
+++ do_links.sh
@@ -2,7 +2,8 @@
 read -n 1 -s -r -p "will remove some files before setting up the sym links, also makes dir ~/.config/nvim. ./dwm/config.h will need to be copied to the dwm directory before doing make & make install. Press any key to continue"
 #TODO can you do these in a loop somehow? need to learn how to bash script...
 mkdir -p ~/.config/nvim
-ln -s ~/.dotfiles/init.vim ~/.config/nvim/
+ln -s ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/.dotfiles/nvim/nvim_cmp_setup.lua ~/.config/nvim/nvim_cmp_setup.lua
 
 bashrcLocation="$HOME/.bashrc"
 rm $bashrcLocation
---
+++ nvim/nvim_cmp_setup.lua
@@ -0,0 +1,99 @@
+local luasnip = require('luasnip')
+local cmp = require('cmp')
+local lspkind = require("lspkind")
+lspkind.init({
+	symbol_map = {
+		Text = "",
+		Method = "",
+		Function = "",
+		Constructor = "",
+		Field = "",
+		Variable = "",
+		Class = "",
+		Interface = "",
+		Module = "",
+		Property = "",
+		Unit = "",
+		Value = "",
+		Enum = "",
+		Keyword = "",
+		Snippet = "",
+		Color = "",
+		File = "",
+		Reference = "",
+		Folder = "",
+		EnumMember = "",
+		Constant = "",
+		Struct = "",
+		Event = "",
+		Operator = "",
+		TypeParameter = ""
+	}
+})
+
+cmp.setup({
+	completion = {
+		completeopt = 'menu,menuone,noinsert',
+	},
+	-- REQUIRED a snippet engine is necessary for auto completion
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = {
+		['<C-p>'] = cmp.mapping.select_prev_item(),
+		--idk if I want or need these yet
+--		['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+--		['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+--		['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+--		['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+--		['<C-e>'] = cmp.mapping({
+--			i = cmp.mapping.abort(),
+--			c = cmp.mapping.close(),
+--		}
+		['<Tab>'] = function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end,
+		['<S-Tab>'] = function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end,
+		['<CR>'] = cmp.mapping.confirm({ select = true });
+	},
+	sources = {
+		{ name = "nvim_lsp" },
+		-- do I need this?
+		{ name = "luasnip" },
+		--{ name = "buffer", keyword_length = 3 },
+	},
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end
+	},
+	formatting = {
+		format = lspkind.cmp_format {
+			with_text = true,
+			menu = {
+				--buffer = "[buf]",
+				nvm_lsp = "[nvm_lsp]",
+				luasnip = "[luasnip]",
+			},
+		}
+	},
+	--experimental = {
+	--	ghost_text = true,
+	--}
+})
