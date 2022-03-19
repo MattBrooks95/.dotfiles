eccafde468af4cfc81fab59911bb024099267860
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-03-19 21:00:23 +0900

    looks finished

--- nvim/init.lua
+++ nvim/init.lua
@@ -73,15 +73,6 @@ local gitMappings = {
 
 setKeyMaps(gitMappings, "n", {})
 
---for _, gitMapping in ipairs(gitMappings) do 
---	vim.api.nvim_set_keymap(
---		"n",
---		gitMapping[1],
---		gitMapping[2],
---		{}
---	)
---end
-
 --TODO this is the cheating way of porting something from viml to lua
 vim.cmd([[
 let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
@@ -141,6 +132,7 @@ vim.api.nvim_exec("autocmd vimenter * ++nested colorscheme everforest", {});
 --TODO the curser only starts blinking after leaving insert mode, it does not blink at the start
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 
+--TODO when I did the init.vim -> init.lua conversion, it seems like I don't need this
 --set runtimepath ^=~/.vim runtimepath +=~/.vim/after
 --vim.opt.runtimepath ="~/.vim runtimepath +=~/.vim/after"
 --let &packpath = &runtimepath
