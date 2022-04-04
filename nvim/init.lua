356fafa4446eef049f54f6dce82f91772632e97f
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-04-04 16:27:00 +0900

    command to resume telescope search

--- nvim/init.lua
+++ nvim/init.lua
@@ -171,7 +171,8 @@ local telescopeMappings = {
 	{ "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>" },
 	{ "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>" },
 	{ "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>" },
-	{ "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>" }
+	{ "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>" },
+	{ "<leader>fr", "<cmd>lua require('telescope.builtin').resume()<cr>" }
 }
 setKeyMaps(telescopeMappings, "n", { noremap=true })
 
