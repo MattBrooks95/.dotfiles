c58b07de3e1fc191f6c63853ab33c9f6562e034e
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-07-03 12:06:23 +0900

    svelte component snippet

--- nvim/lua/snippets.lua
+++ nvim/lua/snippets.lua
@@ -11,13 +11,11 @@ ls.add_snippets("typescript", {
 	s("tern", { i(1), t(" ? "), i(2), t(" : "), i(3), t(";") })
 });
 
+--still looking for a clean way to write snippets that have empty lines
 ls.add_snippets("svelte", {
 	s("comp", {
-		t("<script>"),
-		t("</script>"),
-		t(""),
-		t(""),
-		t("<style>"),
-		t("</style>")
+		t({ "<script>", "</script>", "", "" }),
+		i(1),
+		t({ "", "", "<style>", "</style>" })
 	})
 });
