c28d1ac4df2736a2a01d167b48836f51963c5d60
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-07-03 11:50:04 +0900

    the lua snippets would randomly not show up, or cause errors when expanded. the cause seems to be the way I was declaring them. I was declaring them in one big table, but breaking them out into 'add_snippets' chunks (which previously I could not get to work) seems to have fixed the issue

--- nvim/lua/snippets.lua
+++ nvim/lua/snippets.lua
@@ -3,9 +3,21 @@ local s = ls.snippet;
 local i = ls.insert_node;
 local t = ls.text_node;
 
---todo why can't I use add_snippets?
-ls.snippets = {
-	typescript = {
-		s("tern", { i(1), t(" ? "), i(2), t(" : "), i(3), t(";") })
-	}
-}
+--ls.add_snippets("all", {
+--	s("test", { t("please work") })
+--});
+
+ls.add_snippets("typescript", {
+	s("tern", { i(1), t(" ? "), i(2), t(" : "), i(3), t(";") })
+});
+
+ls.add_snippets("svelte", {
+	s("comp", {
+		t("<script>"),
+		t("</script>"),
+		t(""),
+		t(""),
+		t("<style>"),
+		t("</style>")
+	})
+});
