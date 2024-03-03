d144e94357621f1f30d8e52be40d5ea4b7663e20
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2024-03-03 12:02:06 +0900

    use filetype typescript react, so it shows up in tsx files
    learned how to use function_node to type something once and have it be
    reflected in two different places in the snippet

--- nvim/lua/snippets.lua
+++ nvim/lua/snippets.lua
@@ -1,11 +1,19 @@
 local ls = require("luasnip")
-local s = ls.snippet;
-local i = ls.insert_node;
-local t = ls.text_node;
+local s = ls.snippet
+local i = ls.insert_node
+local t = ls.text_node
+local f = ls.function_node
 
 --ls.add_snippets("all", {
 --	s("test", { t("please work") })
 --});
+
+-- Not sure how to declare lambdas inline, this just gets the string
+-- from the first text node mentioned in the call to function_node
+function getNodeText(arg_nodes)
+	return arg_nodes[1][1]
+end
+
 
 ls.add_snippets("typescript", {
 	s("tern", { i(1), t(" ? "), i(2), t(" : "), i(3), t(";") }),
@@ -34,22 +42,34 @@ ls.add_snippets("typescript", {
 		t({ "", "\t"}), i(4),
 		t({ "", "});"}),
 	}),
-	s("impr", {
-		t("import React from \"react\";")
-	}),
 	s("tstest", {
 		t("test(\""), i(1), t("\", ("), i(2), t(") => {"),
 		t({"", "\t"}), i(3),
 		t({"", "});"}),
 	}),
+});
+
+-- the component name is written in between the const declaration and the
+-- component type, and the prop type name is also written into two places,
+-- next to the function parameter and into the React.FC type declaration
+-- example output, with component name "MyComponent" and prop type name "MyProps"
+--import React from "react";
+--
+--const MyComponent: React.FC<MyProps> = (props: MyProps) => {
+--	return (
+--	);
+--}
+export default MyComponent;
+ls.add_snippets("typescriptreact", {
 	s("reactc", {
-		t("const "), i(1), t(": React.FC"), i(2), t(" = ("), i(3), t(") => {"),
+		t("import React from \"react\";"),
+		t({"", "", "const "}), i(1), t(": React.FC<"), i(2), t("> = (props: "), f(getNodeText, {2}), i(3), t(") => {"),
 		t({"", "\t"}), t("return ("),
 		t({"", "\t);"}),
 		t({"", "}"}),
-		t({"", "export default "}), i(4), t(";")
+		t({"", "export default "}), f(getNodeText, {1}), t(";")
 	})
-});
+})
 
 --still looking for a clean way to write snippets that have empty lines
 ls.add_snippets("svelte", {
