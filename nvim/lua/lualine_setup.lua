c71e7f4ce845cedaaa059baaf4caf24d9a9169bc
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2024-04-18 22:00:09 +0900

    lualine setup specifies icons I have for the diagnostics section

--- nvim/lua/lualine_setup.lua
+++ nvim/lua/lualine_setup.lua
@@ -1,1 +1,20 @@
-require("lualine").setup({})
+-- tip, `:lua vim.print(require('lualine').get_config())` will print the current
+-- lualine configuration to the screen, so you can debug why the bar isn't doing
+-- what you think it should be doing
+require("lualine").setup({
+	sections = {
+		lualine_b = {
+			'branch',
+			'diff',
+			{'diagnostics',
+				symbols = {
+					error = ' ',
+					warn = ' ',
+					info = ' ',
+					hint = ' ',
+				},
+				always_visible = false,
+			}
+		}
+	}
+})
