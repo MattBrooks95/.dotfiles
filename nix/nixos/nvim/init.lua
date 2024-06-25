ad5a6bb99662abb2d157e8447fd54ccab6e40597
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2024-06-25 21:43:25 +0900

    window motions without ctrl+w! so nice!

--- nix/nixos/nvim/init.lua
+++ nix/nixos/nvim/init.lua
@@ -175,7 +175,17 @@ local windowMappings = {
 		{ "<leader>ws", "<cmd>:split<CR>" },
 		-- open a window from a vertical split
 		{ "<leader>wv", "<cmd>:vsplit<CR>" },
+		-- close all windows except for the window that the cursor is in
 		{ "<leader>wo", "<cmd>:only<CR>" },
+		-- close the currently selected window
+		{ "<leader>wc", "<cmd>:close<CR>" },
+
+		-- use wincmd to alias cursor window movements, to avoid needing to type CTRL-w
+		-- it's so nice
+		{ "<leader>wj", "<cmd>:wincmd j<CR>" },
+		{ "<leader>wk", "<cmd>:wincmd k<CR>" },
+		{ "<leader>wh", "<cmd>:wincmd h<CR>" },
+		{ "<leader>wl", "<cmd>:wincmd l<CR>" },
 }
 
 setKeyMaps(windowMappings, "n", { noremap=true, silent=true })
