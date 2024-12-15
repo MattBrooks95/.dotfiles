0ce386e2b50150872efcfb9466c14a406af72170
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2024-12-15 12:14:24 +0900

    rescript tab settings, shiftwidth needs to be 0

--- nix/nixos/nvim/after/ftplugin/rescript.lua
+++ nix/nixos/nvim/after/ftplugin/rescript.lua
@@ -1,5 +1,4 @@
--- use 2 character width tabs for Rescript files
 vim.bo.tabstop = 2
--- setting this to 0 causes vim to use the tabstop value
+-- `shiftwidth` returns `tabstop` value when `shiftwidth` is 0
 vim.bo.shiftwidth = 0
 vim.bo.expandtab = false
