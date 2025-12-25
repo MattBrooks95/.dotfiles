032fe6617b71a6775f2ee5d3ed0ccf6dfab620cf
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2025-12-25 20:14:18 +0900

    remove nvim-treesitter dependency, fix telescope error

--- nix/nixos/nvim/lua/nvim_tree_sitter_setup.lua
+++ nix/nixos/nvim/lua/nvim_tree_sitter_setup.lua
@@ -9,30 +9,35 @@ vim.fn.mkdir(parser_install_dir, "p")
 -- Prevents reinstall of treesitter plugins every boot
 vim.opt.runtimepath:append(parser_install_dir)
 
-local nvim_treesitter = require('nvim-treesitter').install {
-		"haskell", "javascript", "typescript", "lua", "c", "rust",
-		"python", "elixir"
-}
-
-require('nvim-treesitter').setup {
-	--ensure_installed = {
-	--		"haskell",
-	--		"javascript",
-	--		"typescript",
-	--		-- broken grammar, causes errors when I open a rescript file
-	--		-- "rescript",
-	--		"lua",
-	--		"c",
-	--		"rust",
-	--		"ocaml",
-	--		"python",
-	--		"elixir"
-	--},
-	sync_install = false,
-	-- ignore_install = {"javascript"},
-	highlight = {
-		enable = true
-	},
-	-- specify the install dir for the parser files that need to be mutable
-	parser_install_dir = parser_install_dir
-}
+-- I'm not sure what's going on, but nvim-treesitter's `main` branch (the branch
+-- still being developed) causes an error in the Telescope preview
+-- telescope v0.2.0 removes the hard requirement for nvim-treesitter, so I've removed
+-- the configuration here. The error has gone away. I'm not sure how to replace this config
+-- I guess I need to do :h vim.treesitter and do the below configuration using Nvim's API
+-- local nvim_treesitter = require('nvim-treesitter').install {
+-- 		"haskell", "javascript", "typescript", "lua", "c", "rust",
+-- 		"python", "elixir"
+-- }
+-- 
+-- require('nvim-treesitter').setup {
+-- 	--ensure_installed = {
+-- 	--		"haskell",
+-- 	--		"javascript",
+-- 	--		"typescript",
+-- 	--		-- broken grammar, causes errors when I open a rescript file
+-- 	--		-- "rescript",
+-- 	--		"lua",
+-- 	--		"c",
+-- 	--		"rust",
+-- 	--		"ocaml",
+-- 	--		"python",
+-- 	--		"elixir"
+-- 	--},
+-- 	sync_install = false,
+-- 	-- ignore_install = {"javascript"},
+-- 	highlight = {
+-- 		enable = true
+-- 	},
+-- 	-- specify the install dir for the parser files that need to be mutable
+-- 	parser_install_dir = parser_install_dir
+-- }
