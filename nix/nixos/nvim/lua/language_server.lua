93fdf6373cdcc4b406956e069b294830eb785f1b
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2025-12-29 19:20:25 +0900

    Squashed commit of the following:
    commit e02bdeb69dcc1ad5f66422774cd249efabbb0806
    Author: MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
    Date:   Mon Dec 29 19:19:25 2025 +0900
    
        update neovim-flake, so it has treesitter queries
    
    commit 5e1af0dfd8c91e19508f1e667548fd808e47a2f1
    Author: MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
    Date:   Sat Dec 27 23:02:16 2025 +0900
    
        lsp fixed for haskell and rescript, rescript treesitter still has issues
    
    commit 905bf59de8d5c259c226a550178319fd5aa26a63
    Author: MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
    Date:   Sat Dec 27 20:18:37 2025 +0900
    
        work on switching LSP setup to use vim api, requiring lspconfig is deprecated

--- nix/nixos/nvim/lua/language_server.lua
+++ nix/nixos/nvim/lua/language_server.lua
@@ -9,24 +9,30 @@ local servers = {
 	'ruff',
 	'eslint',
 	'rust_analyzer',
-	'rescriptls'
 }
-
-local lspConfig = require('lspconfig')
 
 function setup()
 		for _, lsp in ipairs(servers) do
-			lspConfig[lsp].setup {
+			vim.lsp.config(lsp, {
 				-- on_attach = my_custom_on_attach,
-				capabilities = capabilities,
-			}
+				capabilities = capabilities
+			})
+			vim.lsp.enable(lsp)
 		end
 
+	  vim.lsp.config('rescriptls', {
+				capabilities = capabilities,
+				cmd = {"npx", "@rescript/language-server", "--stdio" }
+		})
+
+		vim.lsp.enable('rescriptls')
+
 		-- need to set some params for the haskell language server
-		lspConfig['hls'].setup {
+		vim.lsp.config('hls', {
 			capabilities = capabilities,
 			cmd = {"haskell-language-server", "--lsp"}
-		}
+		})
+		vim.lsp.enable('hls')
 end
 
 local module = {
