0265a0c20112d3eb2e8f970c1c5e4fd930ea9fa4
matt brooks <28607360+MattBrooks95@users.noreply.github.com>
2026-01-08 18:13:44 +0900

    tell treesitter to use the tsx and typescript parsers for .ts and .tsx files

--- nix/nixos/nvim/after/ftplugin/typescript.lua
+++ nix/nixos/nvim/after/ftplugin/typescript.lua
@@ -1,1 +1,6 @@
+-- https://github.com/nvim-treesitter/nvim-treesitter/blob/9177f2ff061627f0af0f994e3a3c620a84c0c59b/plugin/filetypes.lua#L4
+
+vim.treesitter.language.register("typescript", "ts")
+vim.treesitter.language.register("tsx", { "typescriptreact", "typescript.tsx" })
+
 vim.treesitter.start()
