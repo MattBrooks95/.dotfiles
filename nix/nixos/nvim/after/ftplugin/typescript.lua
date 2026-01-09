e68af6d5d9d7eff8bbaaeb1071aafa4726d7ba1b
matt brooks <28607360+MattBrooks95@users.noreply.github.com>
2026-01-09 10:09:10 +0900

    order

--- nix/nixos/nvim/after/ftplugin/typescript.lua
+++ nix/nixos/nvim/after/ftplugin/typescript.lua
@@ -1,6 +1,10 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter/blob/9177f2ff061627f0af0f994e3a3c620a84c0c59b/plugin/filetypes.lua#L4
 --
 
+vim.treesitter.language.register("typescript", "ts")
+vim.treesitter.language.register("tsx", { "typescriptreact", "typescript.tsx" })
+
+vim.treesitter.start()
 
 --[[
 There are two places where the syntax could be being set to the empty string. I think the typescript and javascript treesitter highlights aren't covering some things that the normal vim syntax does.
@@ -27,8 +31,3 @@ Setting the syntax again here is probably a hack or bandaid over something else 
 ]]
 
 vim.bo.syntax = "typescript";
-
-vim.treesitter.language.register("typescript", "ts")
-vim.treesitter.language.register("tsx", { "typescriptreact", "typescript.tsx" })
-
-vim.treesitter.start()
